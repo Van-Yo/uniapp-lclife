2cb918e3e13528cd8ba485f79b6e58c2bdc57192
wanrong <jokerwanr@gmail.com>
2021-08-11 17:27:25 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,30 @@
+<script>
+	export default {
+		// onLaunch: function() {
+		// 	console.log('App Launch')
+		// },
+		// onShow: function() {
+		// 	console.log('App Show')
+		// },
+		// onHide: function() {
+		// 	console.log('App Hide')
+		// }
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	.page{
+		width: 100%;
+		height: 100%;
+		background-color: #f7f7f7;
+	}
+/* 	uni-page-body,uni-page-refresh{
+		height: 100%;
+	} */
+	.page-block{
+		background-color: #fff;
+		margin-top: 12upx;
+		padding: 20upx;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,49 @@
+<template>
+	<view class="page">
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" class="carousel">
+			<swiper-item v-for="item in swiperList">
+				<view class="swiper-item">
+					<image :src="item" class="carousel"></image>
+				</view>
+			</swiper-item>
+		</swiper>
+		
+		<!-- 热门 -->
+		<view class="page-block">
+			<view class="">
+				<!-- <image src="" mode=""></image> -->
+				<view class="">
+					
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				swiperList :[]
+			}
+		},
+		onLoad() {
+			this.swiperList = [
+				'../../static/swiper/swiper-1.jpg',
+				'../../static/swiper/swiper-2.jpg',
+				'../../static/swiper/swiper-3.jpg'
+			]
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.carousel{
+		width: 100%;
+		height: 400upx;
+	}
+</style>
